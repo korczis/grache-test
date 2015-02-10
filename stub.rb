4310e6f39b85921def5496a5e224496b53a4fe19
Tomas Korcak <korczis@gmail.com>
2015-02-10 14:39:32 +0100

    Improved stub.rb

--- stub.rb
+++ stub.rb
@@ -1,7 +1,20 @@
-# encoding: utf-8
+#! /usr/bin/env ruby
+
+# Gem install grache
+system 'gem install grache --pre' unless system('gem query -i -n grache > /dev/null')
+
+require 'grache'
+
+puts "Using grache version #{Grache::VERSION}"
+
+# Fetch grache pack
+Grache::Packer.new.install()
 
 # Bundler hack
 require 'bundler/cli'
+
+# Install gems required by main.rb
 Bundler::CLI.new.invoke(:install, [], :verbose => false)
 
+# Run main.rb
 Bundler::CLI.new.invoke(:exec, ['ruby', 'main.rb'], :verbose => false)
